a47445e0eeedeafadc051b5059a656832578e251
Noah Schefer <nbschefer@gmail.com>
2019-04-22 14:28:34 -0400

    fixed cat functionality

--- cat.js
+++ cat.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 
 function cat (command) {
-    if (command === 'cat') {
-        process.stdout.write('fileName > ');
-        process.stdin.on('data', (data) => {
-            fs.readFile(data, 'utf8', (err, content) => {
-                if (err) {
-                    throw err;
-                }
-                else {
-                    process.stdout.write(content);
-                }
-            })
+    if (command.includes('cat')) {
+        const fileName = command.split(' ')[1];
+        fs.readFile(fileName, 'utf8', (err, content) => {
+            if (err) {
+                throw err;
+            }
+            else {
+                process.stdout.write(content);
+            }
         })
     }
 }
